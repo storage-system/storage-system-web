e0b184d31db06667f9c6b30a3f96d5f06228834b
Pedro Veras <veraspedro1608@gmail.com>
2024-05-04 16:57:35 -0300

    feat: create fallback root

---
+++ src/utils/fallback.ts
@@ -0,0 +1,10 @@
+export const fallback = (alt: string): string => {
+  alt = alt.trim()
+  if (alt.length <= 3) {
+    return alt
+  }
+
+  const formattedAlt = alt.split(/\s+/).map((word) => word.slice(0, 1))
+  return `${formattedAlt[0]}${formattedAlt.length > 1 ? formattedAlt[formattedAlt.length - 1] : ''
+    }`
+}
