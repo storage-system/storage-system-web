b2111832ca78572c41415168ee12c842f7d816d6
AndreSerafin <aserafin@KSIBRNOT0313.kingspanisoeste.com.br>
2024-07-17 09:01:01 -0300

    feat: git config test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="pt" className={mainFontFamily.variable}>
+    <html className={mainFontFamily.variable}>
       <body>
         <SessionWrapper>
           <QueryClientProvider client={queryClient}>
