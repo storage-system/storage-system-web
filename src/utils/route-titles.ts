14fa79ee49030f030b702a957fee635cfe161d2c
PedroVeras18 <veraspedro1608@gmail.com>
2024-11-20 15:42:47 -0300

    feat: ✨ create new route titles

--- src/utils/route-titles.ts
+++ src/utils/route-titles.ts
@@ -11,4 +11,8 @@ export const routeTitles: RouteTitles = {
   [PrivateRoutes.PROFILE]: 'Perfil',
   [PrivateRoutes.PRODUCT]: 'Produto',
   [PrivateRoutes.CREATE_PRODUCT]: 'Criação de produto',
+  [PrivateRoutes.CONFIGURATIONS]: 'Configurações',
+  [PrivateRoutes.STYLES]: 'Estilos',
+  [PrivateRoutes.CREATE_STYLES]: 'Criação de estilos',
+  [PrivateRoutes.UPDATE_STYLES]: 'Atualização de estilos',
 }
