7dce51c3cbab52734661fb8c207d767f9836ad1c
PedroVeras18 <veraspedro1608@gmail.com>
2025-01-08 18:34:10 -0300

    feat: ✨ add storage logo in sign in and register screen

--- src/app/(public)/register/page.tsx
+++ src/app/(public)/register/page.tsx
@@ -3,11 +3,14 @@ import { RegistrationStepper } from '@/components/routes/register/registration-s
 export default async function RegistrationPage() {
   return (
     <div className="flex h-screen max-h-screen w-screen max-w-[100vw] items-start justify-between">
-      <div>
+      <div className="relative h-screen w-[50vw]">
         <img
           src="./sign-up-bg.png"
-          className="contain h-screen w-[50vw] rounded-br-[120px] bg-contain"
+          className="contain size-full rounded-br-[120px] bg-contain"
         />
+        <h1 className="absolute inset-0 flex items-center justify-center text-4xl font-extrabold text-white">
+          STOR<span className="text-4xl font-normal">AGE</span>
+        </h1>
       </div>
       <div className="flex h-screen w-[50vw] max-w-[50vw] flex-col items-center justify-center space-y-4">
         <div className="w-[30vw] space-y-8">
